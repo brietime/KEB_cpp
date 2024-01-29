f1843a0c1d830c9a7f8cc6128bea4105df8a92e3
cheezeit22 <cheezeit22@inha.edu>
2024-01-29 22:06:00 +0900

    v1.4

--- review/lessson06.cpp
+++ review/lessson06.cpp
@@ -4,31 +4,11 @@ using namespace std;
 
 int main()
 {
-    int a, b, c;
-    double term;
+    const double PI = 3.141582653589793238462;
+    double degree = PI /4;
 
-    cout << "Enter coefficient value a: " ;
-    cin >> a;
-    cout << "Enter coefficient value b: " ;
-    cin >> b;
-    cout << "Enter coefficient value c: " ;
-    cin >> c;
-
-    //discriminant b^2 -4ac
-    term = pow(b, 2) - 4 * a * c;
-    if(term <0)
-    {
-        cout << "no root. " << endl;
-    }
-    else if(term = 0)
-    {
-        cout << "two roots are same." << endl;
-        cout << "x1 = x2 = " << -b/ (2 * a) << endl;
-    }
-    else
-    {
-        cout << "there are two roots which are different. " << endl;
-        cout << "x1 = " << (-b + sqrt(term)) / (2 * a) << endl;
-        cout << "x2 = " << (-b - sqrt(term)) / (2 * a) << endl;
-    }
+    cout << "sin(45): " << sin(degree) << endl;
+    cout << "cos(45): " << cos(degree) << endl;
+    cout << "tan(45): " << tan(degree);
+    return 0;
 }
