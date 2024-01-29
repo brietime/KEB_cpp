01138dae0b13a7b85fc319116a95916e4da66ab8
cheezeit22 <cheezeit22@inha.edu>
2024-01-29 17:11:12 +0900

    v0.4

--- review/lesson02.cpp
+++ review/lesson02.cpp
@@ -3,8 +3,14 @@ using namespace std;
 
 int main()
 {
-    cout << "short int의 크기는 " << sizeof(short int) << "바이트입니다."<< endl;
-    cout << "int의 크기는 " << sizeof(int) << "바이트입니다."<< endl;
-    cout << "long int의 크기는 " << sizeof(long int) << "바이트입니다."<< endl;
+    int x = -1245;
+    unsigned int y = 1245;
+    unsigned int z = -2367;
+    unsigned int t = 14.56;
+
+    cout << x << endl;
+    cout << y << endl;
+    cout << z << endl;
+    cout << t << endl;
     return 0;
 }
