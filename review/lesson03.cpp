130fdd58536088bcb2721e82aef2e31f8efde754
cheezeit22 <cheezeit22@inha.edu>
2024-01-29 18:13:41 +0900

    v1.2

--- review/lesson03.cpp
+++ review/lesson03.cpp
@@ -1,25 +1,19 @@
 #include <iostream>
 using namespace std;
 
-//암묵적 자료형 승격
+//암묵적 자료형 변환(승격처리되는건 변환 x)
 int main()
 {
-    bool x = true;
-    char y = 'A';
-    short z = 14;
-    float t = 24.5;
+    int x = 123;
+    long y = 140;
+    double z = 114.56;
     
-    //bool -> int
-    cout << "x + 100의 자료형: " << typeid(x + 100).name() << endl;
-    cout << "x + 100의 값: " << x + 100 << endl;
-    //char -> int
-    cout << "y + 1000의 자료형: " << typeid(y + 1000).name() << endl;
-    cout << "y + 1000의 값: " << y + 1000 << endl;
-    // short -> int
-    cout << "z + 100의 자료형: " << typeid(z + 100).name() << endl;
-    cout << "z + 100의 값: " << z + 100 << endl;
-    //float -> double
-    cout << "t + 15000.2의 자료형: " << typeid(t + 15000.2).name() << endl;
-    cout << "t + 15000.2의 값: " << t + 15000.2 << endl;
+    //int -> unsigned int -> long -> unsigned long -> double -> long double
+
+    cout << "x + y의 자료형: " << typeid(x + y).name() << endl;
+    cout << "x + y의 값: " << x + y << endl;
+
+    cout << "x + y + z의 자료형: " << typeid(x+y+z).name() << endl;
+    cout << "x + y + z의 값: " << x+y+z << endl;
     return 0;
 }
