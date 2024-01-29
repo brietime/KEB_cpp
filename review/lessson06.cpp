bd2b560d6a99a1a987b3b092aee5c7086bfaf75c
cheezeit22 <cheezeit22@inha.edu>
2024-01-29 22:11:19 +0900

    v1.5

--- review/lessson06.cpp
+++ review/lessson06.cpp
@@ -5,10 +5,24 @@ using namespace std;
 int main()
 {
     const double PI = 3.141582653589793238462;
-    double degree = PI /4;
+    int n;
+    double s, peri, area;
 
-    cout << "sin(45): " << sin(degree) << endl;
-    cout << "cos(45): " << cos(degree) << endl;
-    cout << "tan(45): " << tan(degree);
+    do
+    {
+        cout << "Enter number of polygon sides(more than 3): ";
+        cin >> n;
+    } while (n < 4);
+    
+    do
+    {
+        cout << "Enter length of polygon sides: ";
+        cin >> s;
+    } while (s <= 0.0);
+    
+    peri = n * s;
+    area = (n * pow(s,2))/(n * tan (PI / n));
+    cout << "Perimeter: " << peri << endl;
+    cout << "Area: " << area << endl;
     return 0;
 }
