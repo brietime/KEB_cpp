c4b59875790b1f7d383e252a6538cd197c60a6c1
cheezeit22 <cheezeit22@inha.edu>
2024-01-29 17:49:41 +0900

    v0.8

---
+++ review/lesson03.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << false << "  " << "A" << "  " << "Hello" << endl;
+    cout << 23412 << "  " << 123456789L << endl;
+    cout << 245.83F << "  " << 114.782 << "  " << 2.031L << endl;
+    return 0;
+}
