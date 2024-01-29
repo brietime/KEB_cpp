1950b1cee5c5640961a76cc37d65fb7a7313d873
cheezeit22 <cheezeit22@inha.edu>
2024-01-29 17:56:02 +0900

    v1.0

--- review/lesson03.cpp
+++ review/lesson03.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 using namespace std;
 
+//단순 할당연산자 확인
 int main()
 {
-    int x = 4;
+    int x;
+    int y;
 
-    cout << "괄호가 있는 경우의 값: " << (x + 3) * 5 << endl;
-    cout << "괄호가 없는 경우의 값: " << x + 3 * 5 << endl;
-
-    cout << "괄호가 있는 경우의 값: " << 12 / (x + 2) << endl;
-    cout << "괄호가 없는 경우의 값: " << 12 / x + 2 << endl;
+    cout << "할당 표현식의 리턴값: " <<(x = 14) << endl;
+    cout << "변수 x의 값: " << x << endl;
+    
+    cout << "할당 표현식의 리턴값: " <<(y = 63) << endl;
+    cout << "변수 x의 값: " << y << endl;
     return 0;
 }
